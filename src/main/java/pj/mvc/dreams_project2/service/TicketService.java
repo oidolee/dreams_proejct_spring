7a722e6bafaf7d34a550040a0b1c4f258aacdab6
C.H Kim <ckdgo9409@gmail.com>
2024-02-20 21:02:20 +0900

    수정 완 기능 테스트 90프로 완료

--- src/main/java/pj/mvc/dreams_project2/service/TicketService.java
+++ src/main/java/pj/mvc/dreams_project2/service/TicketService.java
@@ -6,45 +6,47 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.ui.Model;
+
 public interface TicketService {
 
 	// 티켓 목록 조회
-	public void ticketListAction(HttpServletRequest req, HttpServletResponse res) 
+	public void ticketListAction(HttpServletRequest req, Model model) 
 			throws ServletException, IOException;
 	
 	// 티켓 예매 처리
-	public void ticketResAction(HttpServletRequest req, HttpServletResponse res) 
+	public void ticketResAction(HttpServletRequest req, Model model) 
 			throws ServletException, IOException;
 	
 	// 티켓 예매 내역 조회
-	public void resCheckAction(HttpServletRequest req, HttpServletResponse res) 
+	public void resCheckAction(HttpServletRequest req, Model model) 
 			throws ServletException, IOException;
 	
 	// 티켓 예매 취소
-	public void resCancleAction(HttpServletRequest req, HttpServletResponse res) 
+	public void resCancleAction(HttpServletRequest req, Model model) 
 			throws ServletException, IOException;
 	
 	// 티켓 가격 수정
-	public void ticketUpdateAction(HttpServletRequest req, HttpServletResponse res) 
+	public void ticketUpdateAction(HttpServletRequest req, Model model) 
 			throws ServletException, IOException;
 	
 	// 티켓 삭제 처리
-	public void ticketDeleteAction(HttpServletRequest req, HttpServletResponse res)
+	public void ticketDeleteAction(HttpServletRequest req, Model model)
 			throws ServletException, IOException;
 	
 	// 티켓 추가 처리
-	public void ticketInsertAction(HttpServletRequest req, HttpServletResponse res)
+	public void ticketInsertAction(HttpServletRequest req, Model model)
 			throws ServletException, IOException;
 		
 	// 티켓 개별 조회
-	public void ticketEachAction(HttpServletRequest req, HttpServletResponse res) 
+	public void ticketEachAction(HttpServletRequest req, Model model) 
 			throws ServletException, IOException;
 	
 	// 관리자 페이지 티켓 예매 전체내역 조회
-	public void resAdminCheckAction(HttpServletRequest req, HttpServletResponse res) 
+	public void resAdminCheckAction(HttpServletRequest req, Model model) 
 			throws ServletException, IOException;
 	
 	// 관리자 페이지 티켓 예매 상세내역 조회
-	public void resAdminDetailAction(HttpServletRequest req, HttpServletResponse res) 
+	public void resAdminDetailAction(HttpServletRequest req, Model model) 
 			throws ServletException, IOException;
 }
